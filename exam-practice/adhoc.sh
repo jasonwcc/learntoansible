ddc5430acfc989b5b931bb33928af3d546232018
Super User <root@r10s1.example.com>
2026-03-05 08:58:03 +0800

    changes in 202603

--- exam-practice/adhoc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ansible all -m yum_repository -a 'baseurl=https://rhgls.realm13.example.com/BaseOS/ description="EX294 base software" name=EX294-BASE gpgcheck=yes gpgkey=https://rhgls.realm13.example.com/keys/redhat-release enabled=no'
-ansible all -m yum_repository -a 'baseurl=https://rhgls.realm13.example.com/AppStream/ description="EX294 stream software" name=EX294-STREAM gpgcheck=yes gpgkey=https://rhgls.realm13.example.com/keys/redhat-release enabled=no'
